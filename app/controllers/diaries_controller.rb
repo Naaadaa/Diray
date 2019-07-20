23251c18f689e76e14f39eb54f144b1b661310d3
nadaalotaibii <naaadaa_tb@yahoo.com>
2019-07-20 23:00:30 +0300

    adding controller and views

---
+++ app/controllers/diaries_controller.rb
@@ -0,0 +1,34 @@
+class DiariesController < ApplicationController
+    def index
+
+    end 
+
+    def show 
+
+    end 
+
+    def create 
+
+    end 
+
+    def edit
+
+    end 
+
+    def update 
+
+    end 
+
+    def destroy 
+
+    end 
+
+    private 
+
+    def find_diarie
+    end 
+
+    def diarie_params
+
+    end 
+end
