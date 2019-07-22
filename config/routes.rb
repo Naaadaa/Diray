8d2dae8e0f0e62bc07f6e5f3fa9c6efb57abbf5c
nadaalotaibii <naaadaa_tb@yahoo.com>
2019-07-22 12:04:27 +0300

    change the root when user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :diaries
+
+  authenticated :user do
+	  root "diaries#index", as: "authenticated_root" 
+  end
   root 'home#index'
 
-
-resources :diaries
 delete "diaries/:id", to: "diaries#destroy"
 
 end 
